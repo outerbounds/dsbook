d5fffffcc051640dbfa3b253d61cde38c432fe61
Ville Tuulos <tuulos@gmail.com>
2021-12-11 10:09:25 -0800

    fix data_loader for predictions

--- chapter-9/taxi_modules/dnn_data.py
+++ chapter-9/taxi_modules/dnn_data.py
@@ -3,11 +3,12 @@ BATCH_SIZE = 128
 def data_loader(tensor_shards, target=None):
     import tensorflow as tf
     _, dim = tensor_shards[0].shape
+    is_training = target is not None
     def make_batches():
         if target is not None:
             out_tensor = tf.reshape(tf.convert_to_tensor(target),
                                     (len(target), 1))
-        while True:
+        while is_training:
             row = 0
             for shard in tensor_shards:
                 idx = 0
@@ -31,5 +32,5 @@ def data_loader(tensor_shards, target=None):
         signature = (input_sig, tf.TensorSpec(shape=(None, 1)))
     dataset = tf.data.Dataset.from_generator(make_batches,\
                 output_signature=signature)
-    dataset.prefetch(tf.data.AUTOTUNE)
+    data.prefetch(tf.data.AUTOTUNE)
     return input_sig, dataset
