b824ed860fab77801304f15f17774e7605dd0a05
Ville Tuulos <tuulos@gmail.com>
2021-12-11 23:53:55 -0800

    fix data loader

--- chapter-9/taxi_modules/dnn_data.py
+++ chapter-9/taxi_modules/dnn_data.py
@@ -3,8 +3,9 @@ BATCH_SIZE = 128
 def data_loader(tensor_shards, target=None):
     import tensorflow as tf
     _, dim = tensor_shards[0].shape
+    is_training = target is not None
     def make_batches():
-        if target is not None:
+        if is_training:
             out_tensor = tf.reshape(tf.convert_to_tensor(target),
                                     (len(target), 1))
         while True:
@@ -15,7 +16,7 @@ def data_loader(tensor_shards, target=None):
                     x = tf.sparse.slice(shard, [idx, 0], [BATCH_SIZE, dim])
                     n, _ = x.shape
                     if n > 0:
-                        if target is not None:
+                        if is_training:
                             yield x, tf.slice(out_tensor, [row, 0], [n, 1])
                         else:
                             yield x
@@ -23,14 +24,14 @@ def data_loader(tensor_shards, target=None):
                         idx += n
                     else:
                         break
-            if target is not None:
+            if not is_training:
                 break
 
     input_sig = tf.SparseTensorSpec(shape=(None, dim))
-    if target is None:
-        signature = input_sig
-    else:
+    if is_training:
         signature = (input_sig, tf.TensorSpec(shape=(None, 1)))
+    else:
+        signature = input_sig
     dataset = tf.data.Dataset.from_generator(make_batches,\
                 output_signature=signature)
     dataset.prefetch(tf.data.AUTOTUNE)
